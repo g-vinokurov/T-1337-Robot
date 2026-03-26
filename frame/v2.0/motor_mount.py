e1f45499e398ff83e81414f5d5d22bc78c348f7a
g-vinokurov <g.vinokurov@g.nsu.ru>
2026-03-26 21:30:01 +0700

    Add motor mount

---
+++ frame/v2.0/motor_mount.py
@@ -0,0 +1,64 @@
+
+from build123d import *
+from ocp_vscode import show
+
+unit_size           = 8.0 # lego unit size
+unit_height         = 9.6 # lego brick height
+hole_inner_diameter = 4.8 # lego hole inner diameter
+hole_outer_diameter = 6.2 # lego hole outer diameter
+
+with BuildPart() as part:
+    # База
+    length = unit_size * 8
+    width  = unit_size * 4
+    height = unit_height
+    Box(length, width, height)
+    
+    # Отверстия под крепежные винты
+    points = [
+        # Engines
+        (unit_size * 0.5,  unit_size * 1.5,  0),
+        (unit_size * 0.5,  unit_size * -1.5, 0),
+        (unit_size * 2.5,  unit_size * 1.5,  0),
+        (unit_size * 2.5,  unit_size * -1.5, 0),
+        (unit_size * 1.5,  unit_size * 1,    0),
+        (unit_size * 1.5,  unit_size * -1,   0),
+        (unit_size * -0.5, unit_size * 1.5,  0),
+        (unit_size * -0.5, unit_size * -1.5, 0),
+        (unit_size * -2.5, unit_size * 1.5,  0),
+        (unit_size * -2.5, unit_size * -1.5, 0),
+        (unit_size * -1.5, unit_size * 1,    0),
+        (unit_size * -1.5, unit_size * -1,   0),
+    ]
+    for point in points:
+        with BuildPart(Location(point), mode=Mode.SUBTRACT):
+            radius = 1.5
+            height = unit_height
+            align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+            Cylinder(radius, height, align=align)
+    
+    # Утоплялки под крепежные винты
+    z_pos = unit_height / 2 - 1.5
+    points = [
+        # Engines
+        (unit_size * 0.5,  unit_size * 1.5,  z_pos),
+        (unit_size * 0.5,  unit_size * -1.5, z_pos),
+        (unit_size * 2.5,  unit_size * 1.5,  z_pos),
+        (unit_size * 2.5,  unit_size * -1.5, z_pos),
+        (unit_size * 1.5,  unit_size * 1,    z_pos),
+        (unit_size * 1.5,  unit_size * -1,   z_pos),
+    ]
+    for point in points:
+        with BuildPart(Location(point), mode=Mode.SUBTRACT):
+            radius = 3
+            height = 3
+            align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+            Cylinder(radius, height, align=align)
+
+    roundable_edges = part.edges().filter_by(Axis.Z)
+    fillet(roundable_edges, radius=2)
+
+filename = f'{__file__.rstrip('.py')}'
+
+show(part.part, names=[filename])
+export_stl(part.part, f'{filename}.stl')
