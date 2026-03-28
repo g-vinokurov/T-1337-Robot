c108be1d42d6a4c447f381182bda70c36bbfa8ba
g-vinokurov <g.vinokurov@g.nsu.ru>
2026-03-28 20:49:22 +0700

    Add Raspberry Pi 4 box

---
+++ frame/v2.0/raspbery_pi_4.py
@@ -0,0 +1,99 @@
+
+from build123d import *
+from ocp_vscode import show
+
+unit_size           = 8.0 # lego unit size
+unit_height         = 9.6 # lego brick height
+hole_inner_diameter = 4.8 # lego hole inner diameter
+hole_outer_diameter = 6.2 # lego hole outer diameter
+
+with BuildPart() as part:
+    # База
+    length = unit_size * 12
+    width  = unit_size * 9
+    height = unit_height
+    Box(length, width, height)
+
+    # Вырезы
+    x_pos = 0
+    y_pos = 0
+    z_pos = 0
+    with BuildPart(Location((x_pos, y_pos, z_pos)), mode=Mode.SUBTRACT):
+        length = unit_size * 9
+        width  = unit_size * 5
+        height = unit_height
+        align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+        Box(length, width, height)
+
+    # Стенки
+    x_pos = 0
+    y_pos = unit_size * 4 + 1.0
+    z_pos = unit_height * 1.5
+    with BuildPart(Location((x_pos, y_pos, z_pos))):
+        length = unit_size * 12
+        width  = unit_size - 2.0
+        height = unit_height * 2
+        align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+        Box(length, width, height)
+    
+    x_pos = unit_size * 4.5
+    y_pos = unit_size * -4 - 1.0
+    z_pos = unit_height * 1.5
+    with BuildPart(Location((x_pos, y_pos, z_pos))):
+        length = unit_size * 3
+        width  = unit_size - 2.0
+        height = unit_height * 2
+        align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+        Box(length, width, height)
+    
+    x_pos = unit_size * -5.5 - 1.0
+    y_pos = 0
+    z_pos = unit_height * 1.5
+    with BuildPart(Location((x_pos, y_pos, z_pos))):
+        length = unit_size - 2.0
+        width  = unit_size * 9
+        height = unit_height * 2
+        align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+        Box(length, width, height)
+    
+    x_pos = unit_size * -5.5
+    y_pos = 0
+    z_pos = unit_height * 1.5
+    with BuildPart(Location((x_pos, y_pos, z_pos)), mode=Mode.SUBTRACT):
+        length = unit_size
+        width  = unit_size * 2
+        height = unit_height * 2
+        align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+        Box(length, width, height)
+    
+    x_pos = 0
+    y_pos = unit_size * 4
+    z_pos = unit_height * 1.5
+    with BuildPart(Location((x_pos, y_pos, z_pos)), mode=Mode.SUBTRACT):
+        length = unit_size * 6
+        width  = unit_size
+        height = unit_height * 2
+        align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+        Box(length, width, height)
+
+    # Штырики
+    points = [
+        (unit_size * -5 + 3.5,      24.5, unit_height),
+        (unit_size * -5 + 3.5,     -24.5, unit_height),
+        (unit_size * -5 + 3.5 + 58, 24.5, unit_height),
+        (unit_size * -5 + 3.5 + 58,-24.5, unit_height),
+    ]
+    for point in points:
+        with BuildPart(Location(point)):
+            radius = 1.2
+            height = unit_height
+            align  = (Align.CENTER, Align.CENTER, Align.CENTER)
+            Cylinder(radius, height, align=align)
+
+    roundable_edges = part.edges().filter_by(Axis.Z)
+    fillet(roundable_edges, radius=2)
+
+filename = f'{__file__.rstrip('.py')}'
+
+show(part.part, names=[filename])
+export_stl(part.part, f'{filename}.stl')
